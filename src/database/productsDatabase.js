b402deeca33f1ba31e6ba28be26e42d1a3d22706
Camilo López <camiloj.lopez79@gmail.com>
2023-10-12 13:20:12 -0300

    [FEATURE]: Defined data format

---
+++ src/database/productsDatabase.js
@@ -0,0 +1,47 @@
+export const productsDB = [
+  {
+    id: 'p-001',
+    name: '',
+    desc: [
+     { id: '', p: '' },
+     { id: '', p: '' },
+     { id: '', p: '' },
+    ],
+    imgs: [
+      { id: '', name: '', url: '' },
+      { id: '', name: '', url: '' },
+      { id: '', name: '', url: '' },
+    ],
+    slug: '',
+  },
+  {
+    id: 'p-002',
+    name: '',
+    desc: [
+     { id: '', p: '' },
+     { id: '', p: '' },
+     { id: '', p: '' },
+    ],
+    imgs: [
+      { id: '', name: '', url: '' },
+      { id: '', name: '', url: '' },
+      { id: '', name: '', url: '' },
+    ],
+    slug: '',
+  },
+  {
+    id: 'p-003',
+    name: '',
+    desc: [
+     { id: '', p: '' },
+     { id: '', p: '' },
+     { id: '', p: '' },
+    ],
+    imgs: [
+      { id: '', name: '', url: '' },
+      { id: '', name: '', url: '' },
+      { id: '', name: '', url: '' },
+    ],
+    slug: '',
+  },
+];
