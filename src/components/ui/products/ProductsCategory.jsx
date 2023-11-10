2ceb11804f21237ed45b608e7bccaea1c3d52e99
Camilo López <camiloj.lopez79@gmail.com>
2023-11-10 07:55:31 -0300

    [FEATURE]: Changed ProductsPage format

--- src/components/ui/products/ProductsCategory.jsx
+++ src/components/ui/products/ProductsCategory.jsx
@@ -1,5 +1,45 @@
-export const ProductsCategory = () => {
+import {Box, Grid, Typography} from "@mui/material";
+import {ProductCard} from ".";
+
+const testProductData = [
+  { id: '01', name: 'Test Product 1' },
+  { id: '02', name: 'Test Product 2' },
+  { id: '03', name: 'Test Product 3' },
+]
+
+export const ProductsCategory = ({ cateTitle }) => {
   return (
-    <div>ProductsCategory</div>
+    <Box>
+      <Typography 
+        variant='h1'
+        component='h1'
+        textAlign='center'
+      >
+        { cateTitle }
+      </Typography>
+
+      <Grid
+        my={10}
+        pb={ 10 }
+        container
+        spacing={ 4 }
+      >
+        {
+          testProductData.map( ({ id, name }) => (
+            <Grid
+              key={ id }
+              item
+              xs={ 12 }
+              sm={ 4 }
+              display='flex'
+              justifyContent='center'
+              alignItems='center'
+            >
+              <ProductCard />
+            </Grid>
+          ))
+        }
+      </Grid>
+    </Box>
   );
 }
--- src/pages/productos/index.jsx
+++ src/pages/productos/index.jsx
@@ -1,5 +1,6 @@
 // Material UI
 import {
+  Box,
   Grid,
 } from '@mui/material';
 
@@ -7,16 +8,7 @@ import {
 import { ProductCard } from '@/components/ui';
 // Layouts
 import { MainLayout } from '@/components/layouts';
-
-
-const testProductData = [
-  { id: '01', name: 'Test Product 1' },
-  { id: '02', name: 'Test Product 2' },
-  { id: '03', name: 'Test Product 3' },
-  { id: '04', name: 'Test Product 4' },
-  { id: '05', name: 'Test Product 5' },
-  { id: '06', name: 'Test Product 6' },
-]
+import {ProductsCategory} from '@/components/ui/products/ProductsCategory';
 
 
 const ProductsPage = () => {
@@ -24,29 +16,10 @@ const ProductsPage = () => {
     <MainLayout
       pageTitle='Nuestros Productos'
     >
-      <Grid
-        my={10}
-        pb={ 10 }
-        container
-        spacing={ 4 }
-      >
-        {
-          testProductData.map( ({ id, name }) => (
-            <Grid
-              key={ id }
-              item
-              xs={ 12 }
-              sm={ 4 }
-              display='flex'
-              justifyContent='center'
-              alignItems='center'
-            >
-              {/*TODO: Replace for ProductCard*/}
-              <ProductCard />
-            </Grid>
-          ))
-        }
-      </Grid>
+      <Box pt={ 7 } />
+      <ProductsCategory cateTitle='Dispositivos GNSS X5' />
+      <ProductsCategory cateTitle='Software y Servicios' />
+      <ProductsCategory cateTitle='Combos' />
     </MainLayout>
   );
 }
