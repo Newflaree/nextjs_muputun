a3edc907782115622ef3641cd7aeb5f0890c078a
Camilo López <camiloj.lopez79@gmail.com>
2023-11-17 13:20:48 -0300

    [FEATURE]: Implemented new bg headers

--- src/database/headerDatabase.js
+++ src/database/headerDatabase.js
@@ -5,35 +5,35 @@ export const headerInfo = [
     desc: 'Rápida precisión centimétrica. Configuración por Bluetooth y WiFi. Cuenta con el servicio NTRIP LOCAL para enviar correcciones a Drones RTK de las marcas DJI Enterprise, Wingtra, eBee y Autel sin necesidad de conexión a Internet.',
     btn: 'Ir al X5 Receptor',
     path: 'productos/x5-receptor',
-    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1694637816/demos/muputun/Copia_de_Copia_de_X5_RECEIVER_-_EN_CAMPO_-02_c4sm84.png',
-    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1694637816/demos/muputun/Copia_de_Copia_de_X5_RECEIVER_-_EN_CAMPO_-02_c4sm84.png'
+    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png',
+    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png'
   },
   {
     id: 'hi-004',
-    title: 'GNSS X5 Mobile Series',
+    title: 'X5 GNSS Mobile Series',
     desc: 'Mini Receptores Multibanda GNSS de alta precisión para Celulares, Tablets y Laptops. Sistemas de corrección PPK, NTRIP (y RTK en la versión PRO) a través de conexión USB O Bluetooth.',
     btn: 'Ir al GNSS X5',
     path: 'productos/x5-mobile-series',
-    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1700177032/demos/muputun/nnn8xasjvt696zdkxm4x.jpg',
-    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1700177032/demos/muputun/nnn8xasjvt696zdkxm4x.jpg'
+    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png',
+    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png'
   },
   {
     id: 'hi-002',
-    title: 'Conece nuestros productos',
-    desc: 'Da el siguiente paso, Revisa el catágolo que ofrecemos para simplificar tus métodos de trabajo en base a equipos con tecnologías al alcance de todos.',
-    btn: 'Ir a Productos',
-    path: 'productos',
-    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1694637245/demos/muputun/Copia_de_20230210_131928_aq1hro.jpg',
-    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1694637245/demos/muputun/Copia_de_20230210_131928_aq1hro.jpg'
+    title: 'X5 PPK Phantom 4 Pro',
+    desc: 'El Módulo X5 PPK para Phantom 4 Pro es un sistema adaptador GNSS no intrusivo para conectarse a un Drone DJI Phantom 4 Pro v.1/v.2 o Advance',
+    btn: 'Ir a X5 PPK P4P',
+    path: 'productos/x5-ppk-phantom-4-pro',
+    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png',
+    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png'
   },
   {
     id: 'hi-003',
-    title: '¿Quieres más información?',
-    desc: 'No dudes en contactarnos, somos profesionales amantes de la tecnología, capacitados para dar soluciones a tus requerimientos y con gusto atenderemos tus consultas o inquietudes con la calidez y dedicación que mereces, enfocados en cumplir tus expectativas.',
-    btn: 'Contáctanos',
-    path: 'contacto',
-    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1694637636/demos/muputun/Copia_de_Copia_de_20230210_124315_pmjcb7.jpg',
-    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1694637636/demos/muputun/Copia_de_Copia_de_20230210_124315_pmjcb7.jpg'
+    title: 'SurPad 4.2 GNSS',
+    desc: 'SurPad es una aplicación para Android diseñada para profesionales que utilizan receptores GPS/GNSS RTK. Es especialmente útil para topógrafos, cartógrafos, ingenieros, arquitectos y planificadores urbanos',
+    btn: 'Ir a Aplicación SurPad',
+    path: 'productos/surpad-4.2-gnss-app',
+    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png',
+    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png'
   },
   {
     id: 'hi-001',
@@ -41,7 +41,7 @@ export const headerInfo = [
     desc: 'Creemos que la innovación que surge de la fusión de diferentes tecnologías puede acelerar nuestra evolución y llevarnos a una mejor calidad de vida, más práctica, más sencilla y eficiente',
     btn: 'Ir a Mettatec',
     path: 'https://mettatec.com/es/',
-    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1694637238/demos/muputun/Copia_de_X5_MOBILE_-_METTA_vinculado_a_07_00427_wwrzrn.jpg',
-    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1694637238/demos/muputun/Copia_de_X5_MOBILE_-_METTA_vinculado_a_07_00427_wwrzrn.jpg'
+    imgd: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1080,w_1920/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png',
+    imgm: 'https://res.cloudinary.com/newflare/image/upload/c_fill,h_1920,w_720/v1700237815/demos/muputun/kqm9os4tfnjyyng9lgmv.png'
   },
 ];
